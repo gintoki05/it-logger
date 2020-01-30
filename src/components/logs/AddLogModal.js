4143d4311f6ce4c64a4df4f1ba9f7228353eeb92
gintoki <ajieprastyo@gmail.com>
2020-01-30 10:44:32 +0700

    finish course

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addLog } from '../../actions/logActions';
+import TechSelectOptions from '../techs/TechSelectOptions';
 
 import M from 'materialize-css/dist/js/materialize.min.js';
 
@@ -61,9 +62,7 @@ const AddLogModal = ({ addLog }) => {
             <option value="" disabled>
               Select Technician
             </option>
-            <option value="John Doe">John Doe</option>
-            <option value="Deep">Deep</option>
-            <option value="Marry">Marry</option>
+            <TechSelectOptions />
           </select>
         </div>
 
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -31,34 +31,34 @@ export default (state = initialState, action) => {
         logs: [...state.logs, action.payload],
         loading: false
       };
-    // case DELETE_LOG:
-    //   return {
-    //     ...state,
-    //     logs: state.logs.filter(log => log.id !== action.payload),
-    //     loading: false
-    //   };
-    // case UPDATE_LOG:
-    //   return {
-    //     ...state,
-    //     logs: state.logs.map(log =>
-    //       log.id === action.payload.id ? action.payload : log
-    //     )
-    //   };
-    // case SEARCH_LOGS:
-    //   return {
-    //     ...state,
-    //     logs: action.payload
-    //   };
-    // case SET_CURRENT:
-    //   return {
-    //     ...state,
-    //     current: action.payload
-    //   };
-    // case CLEAR_CURRENT:
-    //   return {
-    //     ...state,
-    //     current: null
-    //   };
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter(log => log.id !== action.payload),
+        loading: false
+      };
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.map(log =>
+          log.id === action.payload.id ? action.payload : log
+        )
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null
+      };
     case SET_LOADING:
       return {
         ...state,
